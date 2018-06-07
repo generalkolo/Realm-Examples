0bef3d251c0fa722911b4cd99feea00c3374f8b1
Edet Ebenezer <segunebenezer72@gmail.com>
2018-06-07 22:52:48 +0100

    Encrypted realm tutorial example codes added

---
+++ Encrypted Realm/app/src/main/java/com/semanientreprise/encryptedrealm/MainActivity.java
@@ -0,0 +1,79 @@
+package com.semanientreprise.encryptedrealm;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.SecureRandom;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+import io.realm.RealmResults;
+
+public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.countriesRecView)
+    RecyclerView countriesRecView;
+    private Realm realm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Realm.init(this);
+
+        byte[] key = new byte[64];
+        new SecureRandom().nextBytes(key);
+
+        RealmConfiguration encryptedConfiguration = new RealmConfiguration.Builder()
+                .encryptionKey(key)
+                .name("realEncrypted.realm")
+                .build();
+
+        Realm.deleteRealm(encryptedConfiguration);
+
+        // Open the Realm with encryption enabled
+        realm = Realm.getInstance(encryptedConfiguration);
+
+        fillUpCountriesRealm();
+
+        realm.beginTransaction();
+
+        final RealmResults<Countries> countriesResults = realm.where(Countries.class).findAll();
+
+        realm.commitTransaction();
+
+        countriesRecView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        countriesRecView.setHasFixedSize(true);
+
+        countriesRecView.setAdapter(new MyAdapter(countriesResults));
+
+    }
+
+    private void fillUpCountriesRealm() {
+        realm.executeTransaction(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                InputStream inputStream = getResources().openRawResource(R.raw.countries);
+                try {
+                    realm.createAllFromJson(Countries.class, inputStream);
+                } catch (IOException e) {
+                    if (realm.isInTransaction())
+                        realm.cancelTransaction();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        realm.close();
+    }
+}
