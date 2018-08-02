690e5cf5123364e96683f8437d47a8f174e1ce90
Edet Ebenezer <segunebenezer72@gmail.com>
2018-08-02 16:25:10 +0100

    deleted uncessary files

--- app/src/main/java/com/semanientreprise/realmtoroom/UserDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.semanientreprise.realmtoroom;
-
-import android.arch.persistence.room.Database;
-import android.arch.persistence.room.RoomDatabase;
-
-@Database(entities = {User.class},version = 1)
-public abstract class UserDatabase extends RoomDatabase{
-    public abstract UserDao personDao();
-}
